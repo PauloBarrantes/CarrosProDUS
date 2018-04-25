0d7d4751f1c45764c72bbdde6b4a60a193345d9e
paulobarrantes <paulobarrantes98@gmail.com>
2018-04-25 15:19:00 -0600

    Controller and views

--- src/main/java/application/controller/Controller.java
+++ src/main/java/application/controller/Controller.java
@@ -1,18 +1,36 @@
 package application.controller;
 
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     @RequestMapping(value = "/")
     public String getIndex() {
         return "index";
+    }
+    @RequestMapping(value="/mantenimiento")
+    public String mantenimiento(){
 
+        return "mantenimiento";
+    }
+
+    @RequestMapping(value="/reservacion")
+    public String reservacion(){
+        return "reservacion";
+    }
+    @RequestMapping(value="/control")
+    public String control(){
+        return "control";
+    }
+
+    @RequestMapping(value="/login")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping(value="/reservaciones")
+    public String reservaciones(){
+        return "reservaciones";
     }
 }
